93b524256349c77a4513734b968ebca3904b4d7e
Lee Priest <lee@leepriest.dev>
2025-05-22 23:50:06 +0100

    Update stack logical id

--- bin/app.ts
+++ bin/app.ts
@@ -5,7 +5,7 @@ import { ServiceEventBridgeStack } from "../cdk/stacks/ServiceEventbridgeStack";
 const stage = process.env.STAGE || "dev";
 
 const app = new cdk.App();
-new ServiceEventBridgeStack(app, `ServiceEventBridgeStack-${stage}`, {
+new ServiceEventBridgeStack(app, `service-event-bridge-stack-${stage}`, {
 	stage,
 	serviceName: "service-eventbridge",
 	eventBusName: "service-eventbridge-event-bus",
